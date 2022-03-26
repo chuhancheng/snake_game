6c0adc9fe9bdfbd59e55dea937ab3acf8da4fc10
Darren Cheng <chuhancheng@gmail.com>
2022-03-26 20:49:33 +0800

    init project

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,25 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+//   output: {
+//     filename: "main.js",
+//     path: path.resolve(__dirname, "dist"),
+//   },
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
